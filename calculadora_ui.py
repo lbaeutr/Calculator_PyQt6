89f81cb5c98d8706825002989d812ec854b5c84b
lbaeutr <lbaeutr@gmail.com>
2025-01-30 19:37:20 +0100

    Pequeño avance

--- calculadora_ui.py
+++ calculadora_ui.py
@@ -23,10 +23,11 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 500)
+        MainWindow.resize(801, 501)
         font = QFont()
         font.setPointSize(20)
         MainWindow.setFont(font)
+        MainWindow.setCursor(QCursor(Qt.CursorShape.CrossCursor))
         MainWindow.setStyleSheet(u"QMainWindow {\n"
 "    background-color: #121212;\n"
 "}\n"
@@ -35,7 +36,7 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.display = QLineEdit(self.centralwidget)
         self.display.setObjectName(u"display")
-        self.display.setGeometry(QRect(20, 20, 321, 81))
+        self.display.setGeometry(QRect(20, 50, 411, 121))
         font1 = QFont()
         self.display.setFont(font1)
         self.display.setStyleSheet(u"QLineEdit {\n"
@@ -48,11 +49,25 @@ class Ui_MainWindow(object):
 "")
         self.display.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
         self.table_history = QTableWidget(self.centralwidget)
-        if (self.table_history.columnCount() < 2):
-            self.table_history.setColumnCount(2)
+        if (self.table_history.columnCount() < 3):
+            self.table_history.setColumnCount(3)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.table_history.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.table_history.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.table_history.setHorizontalHeaderItem(2, __qtablewidgetitem2)
         self.table_history.setObjectName(u"table_history")
+        self.table_history.setEnabled(True)
         self.table_history.setGeometry(QRect(450, 20, 341, 471))
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.table_history.sizePolicy().hasHeightForWidth())
+        self.table_history.setSizePolicy(sizePolicy)
         self.table_history.setFont(font1)
+        self.table_history.viewport().setProperty(u"cursor", QCursor(Qt.CursorShape.CrossCursor))
+        self.table_history.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         self.table_history.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
         self.table_history.setStyleSheet(u"QTableWidget {\n"
 "    background-color: #121212;\n"
@@ -60,6 +75,7 @@ class Ui_MainWindow(object):
 "    gridline-color: transparent;\n"
 "    border: none;\n"
 "    font-size: 20px;\n"
+"	text-align: center;\n"
 "}\n"
 "\n"
 "QTableWidget::item {\n"
@@ -67,13 +83,31 @@ class Ui_MainWindow(object):
 "    font-weight: bold;\n"
 "    color: #FFA726;\n"
 "}\n"
+"\n"
+"QHeaderView::section {\n"
+"    background-color: #121212;  \n"
+"    color: white;               \n"
+"    font-size: 18px;\n"
+"    font-weight: bold;\n"
+"    border: none;\n"
+"}\n"
+"\n"
 "")
-        self.table_history.setColumnCount(2)
-        self.table_history.horizontalHeader().setVisible(False)
-        self.widget = QWidget(self.centralwidget)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(10, 150, 431, 336))
-        self.widget.setStyleSheet(u"QPushButton {\n"
+        self.table_history.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.table_history.setAutoScroll(False)
+        self.table_history.setGridStyle(Qt.PenStyle.SolidLine)
+        self.table_history.setWordWrap(True)
+        self.table_history.setRowCount(0)
+        self.table_history.setColumnCount(3)
+        self.table_history.horizontalHeader().setVisible(True)
+        self.table_history.horizontalHeader().setDefaultSectionSize(114)
+        self.table_history.horizontalHeader().setStretchLastSection(True)
+        self.table_history.verticalHeader().setVisible(False)
+        self.table_history.verticalHeader().setCascadingSectionResizes(True)
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 150, 431, 336))
+        self.layoutWidget.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
 "    color: white;\n"
 "    border: none;\n"
@@ -105,10 +139,10 @@ class Ui_MainWindow(object):
 "    color: white;\n"
 "}\n"
 "")
-        self.gridLayout = QGridLayout(self.widget)
+        self.gridLayout = QGridLayout(self.layoutWidget)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.btn_7 = QPushButton(self.widget)
+        self.btn_7 = QPushButton(self.layoutWidget)
         self.btn_7.setObjectName(u"btn_7")
         self.btn_7.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -145,7 +179,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_7, 0, 0, 1, 1)
 
-        self.btn_8 = QPushButton(self.widget)
+        self.btn_8 = QPushButton(self.layoutWidget)
         self.btn_8.setObjectName(u"btn_8")
         self.btn_8.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -182,7 +216,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_8, 0, 1, 1, 1)
 
-        self.btn_9 = QPushButton(self.widget)
+        self.btn_9 = QPushButton(self.layoutWidget)
         self.btn_9.setObjectName(u"btn_9")
         self.btn_9.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -219,7 +253,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_9, 0, 2, 1, 1)
 
-        self.btn_borrar_ultimo_numero = QPushButton(self.widget)
+        self.btn_borrar_ultimo_numero = QPushButton(self.layoutWidget)
         self.btn_borrar_ultimo_numero.setObjectName(u"btn_borrar_ultimo_numero")
         self.btn_borrar_ultimo_numero.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -256,7 +290,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_borrar_ultimo_numero, 0, 3, 1, 1)
 
-        self.btn_4 = QPushButton(self.widget)
+        self.btn_4 = QPushButton(self.layoutWidget)
         self.btn_4.setObjectName(u"btn_4")
         self.btn_4.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -293,7 +327,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_4, 1, 0, 1, 1)
 
-        self.btn_5 = QPushButton(self.widget)
+        self.btn_5 = QPushButton(self.layoutWidget)
         self.btn_5.setObjectName(u"btn_5")
         self.btn_5.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -330,7 +364,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_5, 1, 1, 1, 1)
 
-        self.btn_6 = QPushButton(self.widget)
+        self.btn_6 = QPushButton(self.layoutWidget)
         self.btn_6.setObjectName(u"btn_6")
         self.btn_6.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -367,7 +401,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_6, 1, 2, 1, 1)
 
-        self.btn_resta = QPushButton(self.widget)
+        self.btn_resta = QPushButton(self.layoutWidget)
         self.btn_resta.setObjectName(u"btn_resta")
         self.btn_resta.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -404,7 +438,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_resta, 1, 3, 1, 1)
 
-        self.btn_1 = QPushButton(self.widget)
+        self.btn_1 = QPushButton(self.layoutWidget)
         self.btn_1.setObjectName(u"btn_1")
         self.btn_1.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -441,7 +475,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_1, 2, 0, 1, 1)
 
-        self.btn_2 = QPushButton(self.widget)
+        self.btn_2 = QPushButton(self.layoutWidget)
         self.btn_2.setObjectName(u"btn_2")
         self.btn_2.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -478,7 +512,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_2, 2, 1, 1, 1)
 
-        self.btn_3 = QPushButton(self.widget)
+        self.btn_3 = QPushButton(self.layoutWidget)
         self.btn_3.setObjectName(u"btn_3")
         self.btn_3.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -515,7 +549,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_3, 2, 2, 1, 1)
 
-        self.btn_suma = QPushButton(self.widget)
+        self.btn_suma = QPushButton(self.layoutWidget)
         self.btn_suma.setObjectName(u"btn_suma")
         self.btn_suma.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -552,7 +586,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_suma, 2, 3, 1, 1)
 
-        self.btn_coma = QPushButton(self.widget)
+        self.btn_coma = QPushButton(self.layoutWidget)
         self.btn_coma.setObjectName(u"btn_coma")
         self.btn_coma.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -589,7 +623,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_coma, 3, 0, 1, 1)
 
-        self.btn_0 = QPushButton(self.widget)
+        self.btn_0 = QPushButton(self.layoutWidget)
         self.btn_0.setObjectName(u"btn_0")
         font2 = QFont()
         font2.setBold(True)
@@ -629,7 +663,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_0, 3, 1, 1, 1)
 
-        self.btn_dividir = QPushButton(self.widget)
+        self.btn_dividir = QPushButton(self.layoutWidget)
         self.btn_dividir.setObjectName(u"btn_dividir")
         self.btn_dividir.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -666,7 +700,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_dividir, 3, 2, 1, 1)
 
-        self.btn_multiplicacion = QPushButton(self.widget)
+        self.btn_multiplicacion = QPushButton(self.layoutWidget)
         self.btn_multiplicacion.setObjectName(u"btn_multiplicacion")
         self.btn_multiplicacion.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -703,7 +737,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_multiplicacion, 3, 3, 1, 1)
 
-        self.btn_clear = QPushButton(self.widget)
+        self.btn_clear = QPushButton(self.layoutWidget)
         self.btn_clear.setObjectName(u"btn_clear")
         self.btn_clear.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -740,7 +774,7 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.btn_clear, 4, 0, 1, 2)
 
-        self.btn_igual = QPushButton(self.widget)
+        self.btn_igual = QPushButton(self.layoutWidget)
         self.btn_igual.setObjectName(u"btn_igual")
         self.btn_igual.setStyleSheet(u"QPushButton {\n"
 "    background-color: #1E1E1E;\n"
@@ -786,6 +820,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        ___qtablewidgetitem = self.table_history.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"Operaci\u00f3n", None));
+        ___qtablewidgetitem1 = self.table_history.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"C\u00e1lculo", None));
+        ___qtablewidgetitem2 = self.table_history.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"Resultado", None));
         self.btn_7.setText(QCoreApplication.translate("MainWindow", u"7", None))
         self.btn_8.setText(QCoreApplication.translate("MainWindow", u"8", None))
         self.btn_9.setText(QCoreApplication.translate("MainWindow", u"9", None))
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class pantalla(QMainWindow):
         uic.loadUi("calculadora.ui", self)
 
         self.id_operacion = 1  # Contador para el historial
+        self.display.setText("0")  # Inicializar el display en 0
 
         # Conectar los botones con las funciones
         self.setup_buttons()
@@ -18,28 +19,32 @@ class pantalla(QMainWindow):
             getattr(self, f"btn_{i}").clicked.connect(lambda _, num=i: self.add_to_display(str(num)))
 
         # Botones de operadores
-       # ...existing code...
         self.btn_suma.clicked.connect(lambda: self.add_to_display("+"))
         self.btn_resta.clicked.connect(lambda: self.add_to_display("-"))
         self.btn_multiplicacion.clicked.connect(lambda: self.add_to_display("*"))
-        self.btn_dividir.clicked.connect(lambda: self.add_to_display("÷"))
+        self.btn_dividir.clicked.connect(lambda: self.add_to_display("/"))
         self.btn_coma.clicked.connect(lambda: self.add_to_display("."))
         self.btn_igual.clicked.connect(self.calculate)
         self.btn_borrar_ultimo_numero.clicked.connect(self.delete_last_character)
         self.btn_clear.clicked.connect(self.clear_display)
-# ...existing code...
 
 
     def add_to_display(self, text):
         current = self.display.text()
-        self.display.setText(current + text)
+        if current == "0":
+            self.display.setText(text)
+        else:   
+            self.display.setText(current + text)
 
     def clear_display(self):
-        self.display.clear()
+        self.display.setText("0")
 
     def delete_last_character(self):
         current = self.display.text()
-        self.display.setText(current[:-1])
+        if len(current) == 1:
+            self.display.setText("0")
+        else:
+            self.display.setText(current[:-1])
 
     def calculate(self):
         try:
@@ -54,9 +59,13 @@ class pantalla(QMainWindow):
     def add_to_history(self, operation, result):
         row_position = self.table_history.rowCount()
         self.table_history.insertRow(row_position)
-        self.table_history.setItem(row_position, 0, QTableWidgetItem(str(self.id_operacion)))
+        #self.table_history.setItem(row_position, 0, QTableWidgetItem(str(self.id_operacion)))
+        #self.table_history.setItem(row_position, 1, QTableWidgetItem(operation))
+        #self.table_history.setItem(row_position, 2, QTableWidgetItem(str(result)))
         self.table_history.setItem(row_position, 1, QTableWidgetItem(operation))
         self.table_history.setItem(row_position, 2, QTableWidgetItem(str(result)))
+        self.table_history.setItem(row_position, 0, QTableWidgetItem(str(self.id_operacion)))
+       
         self.id_operacion += 1
 
     def show_error(self, message):
